e0a7cc03d6aeb7f5a3535c6300bd956a53ce7006
divineajaegbu4 <divineajaegbu4@gmail.com>
2025-06-25 00:45:33 +0100

    Add javascript specials

---
+++ javascript/javascript-fundamentals/javascript-specials.js
@@ -0,0 +1,9 @@
+console.log("===========Javascript Specials========");
+// Semicolons are not required after code blocks {...} and syntax constructs with them like loops:
+function f() {
+  // no semicolon needed after function declaration
+}
+
+for(;;) {
+  // no semicolon needed after the loop
+}
